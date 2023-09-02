a1fd8e412e089877645ded20e5615e2c29d34265
Chapelk1 <klepach154789@gmail.com>
2023-09-02 19:30:52 +0300

    +

---
+++ src/components/app.jsx
@@ -0,0 +1,20 @@
+import { Profile } from 'components/Profile/Profile';
+import userInfo from 'user.json'
+
+
+
+export const App = () => {
+    return (
+    <div>
+        <Profile
+            username={userInfo.username}
+            tag={userInfo.tag}
+            location={userInfo.location}
+            avatar={userInfo.avatar}
+            followers={userInfo.stats.followers}
+            views={userInfo.stats.views}
+            likes={userInfo.stats.likes}
+        />
+    </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom';
- 
+import {App} from './components/App.jsx'
 
 
 
+ReactDOM.render(<App />
+  , document.querySelector('#root'))
